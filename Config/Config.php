4b7b178c5fce3c265965716db09292ba11431c35
songyang <475511896@qq.com>
2019-04-27 09:31:07 +0800

    ddd

---
+++ Config/Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-04-27
+ * Time: 9:20
+ */
+
+/**
+ * 框架惯例配置
+ */
+$config = [
+    "Web" => [
+        "Controller_Def" => "Index", //默认控制器
+        "Function_Def" => "index", //默认方法
+        "TimeZone" => "PRC", //默认时区 （中华人民共和国 东八区）
+        "Controller_Name" => "c", //默认控制器url参数名
+        "Function_Name" => "f", //默认方法url参数名
+    ]
+];
---
+++ Config/DbConfig.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-04-27
+ * Time: 9:27
+ */
+
+$Dbconfig = [
+    "Db_HOST" => "",
+    "Db_USER" => "",
+    "Db_PASS" => "",
+    "Db_NAME" => "",
+];
