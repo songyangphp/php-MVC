2e97acc7ba530bacd3e93d851e259bc1bc60cfd1
songyang <475511896@qq.com>
2019-08-29 11:34:35 +0800

    songyang commit

--- index.php
+++ index.php
@@ -13,23 +13,4 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 include_once "Run.php";
 
-function loopArrayWriteFile($array, $file, $no = 0)
-{
-    $j = $no;
-    foreach ($array as $k => $v){
-        $space = "";
-        for ($i = 0; $i <= $no; $i++){
-            $space .= "   ";
-        }
-
-        if(is_array($v)){
-            file_put_contents($file,$space . $k . " => ".PHP_EOL , FILE_APPEND | LOCK_EX);
-            $this->loopArrayWriteFile($v, $file, $j+1);
-            continue;
-        }
-
-        file_put_contents($file,$space . $k . " => ". $v.PHP_EOL , FILE_APPEND | LOCK_EX);
-    }
-}
-
 (new Run())->initApp()->initController()->initModel()->appRun();
